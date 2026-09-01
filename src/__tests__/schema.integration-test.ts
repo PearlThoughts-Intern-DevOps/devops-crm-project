084fbffc2ff2d960280a3326ac27259d21b51a0a
reddy8897 <ambureddy.1615@gmail.com>
2026-09-01 16:35:46 +0530

    Fix nullable integration test response

--- src/__tests__/schema.integration-test.ts
+++ src/__tests__/schema.integration-test.ts
@@ -34,7 +34,11 @@ describe('CoreApiClient', () => {
         id: true,
       },
     });
-    expect(created.createNote.id).toBeDefined();
+    expect(created.createNote).toBeDefined();
+
+    if (!created.createNote) {
+      throw new Error('The note was not created');
+    }
 
     await client.mutation({
       destroyNote: {
