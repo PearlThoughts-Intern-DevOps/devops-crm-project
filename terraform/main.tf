460ffde5fb7c1375fd0bbc76df906153b53b4c9b
Varad Ahir <ahirv8351@gmail.com>
2026-09-08 14:55:04 +0530

    feat: add Terraform configuration for Twenty CRM infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "twenty_crm" {
+  name        = "${var.instance_name}-sg"
+  description = "Security group for Twenty CRM EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Twenty CRM"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.instance_name}-sg"
+  }
+}
+
+resource "aws_instance" "twenty_crm" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default_vpc.ids[0]
+  vpc_security_group_ids      = [aws_security_group.twenty_crm.id]
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = var.instance_name
+  }
+}
