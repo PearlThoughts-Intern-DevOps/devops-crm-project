6de6302df24a16c355053cc2a2761a03535abbdf
rohithh55 <rohiithh5@gmail.com>
2026-09-08 15:01:46 +0530

    Add EC2 instance and security group resources

---
+++ Terraform/modules/ec2/main.tf
@@ -0,0 +1,58 @@
+resource "aws_security_group" "twenty_crm" {
+  name   = "${var.project_name}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2020
+    to_port     = 2020
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+resource "aws_instance" "twenty_crm" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+
+  vpc_security_group_ids = [
+    aws_security_group.twenty_crm.id
+  ]
+
+  tags = {
+    Name = var.project_name
+  }
+}
