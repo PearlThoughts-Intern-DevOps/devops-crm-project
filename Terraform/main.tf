ca803c0afc03d70b960546409d7d235e8648430f
rohithh55 <rohiithh5@gmail.com>
2026-09-08 14:57:36 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  project_name     = var.project_name
+  ami_id           = var.ec2_ami_id
+  instance_type    = var.ec2_instance_type
+  key_name         = var.ec2_key_name
+  vpc_id           = module.vpc.vpc_id
+  subnet_id        = module.vpc.subnet_id
+  application_port = var.twenty_crm_port
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+
+  project_name    = var.project_name
+  repository_name = var.ecr_repository_name
+}
