2cc479d4844685ebf01ee4f675b7ed25e33ab018
rohithh55 <rohiithh5@gmail.com>
2026-09-10 14:53:14 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "aws_region" {
+  description = "AWS region for the Twenty CRM infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "twenty-crm"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID for the Twenty CRM EC2 instance"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ec2_key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "twenty_crm_port" {
+  description = "Port used by the Twenty CRM application"
+  type        = number
+  default     = 2020
+}
+
+variable "ecr_repository_name" {
+  description = "Amazon ECR repository name"
+  type        = string
+  default     = "twenty-crm"
+}
+
+variable "iam_instance_profile" {
+  type = string
+}
