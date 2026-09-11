24242f414be964e525d51a144eb90af3387925c2
moechadSayshi <mohitsinghwiththeglasses@protonmail.com>
2026-09-11 09:44:51 +0530

    task 13 output

--- terraform/output.tf
+++ terraform/output.tf
@@ -47,8 +47,3 @@ output "twenty_crm_url_2020" {
   description = "URL to access Twenty CRM on port 2020"
   value       = "http://${aws_instance.twenty_crm.public_ip}:2020"
 }
-
-output "twenty_crm_url_3000" {
-  description = "URL to access Twenty CRM on port 3000"
-  value       = "http://${aws_instance.twenty_crm.public_ip}:3000"
-}
