15e231ede467502d30306c0ffb9e6d94b0d329bc
Puneet rathore <rathorepuneet99@gmail.com>
2026-09-15 12:30:44 +0530

    Add Task 15 ALB Terraform configuration

---
+++ Task-15-ALB/variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "Approved Ubuntu AMI"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Existing EC2 Key Pair name"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR allowed to SSH into EC2"
+  type        = string
+}
+
+variable "docker_image" {
+  description = "Twenty CRM Docker image"
+  type        = string
+}
