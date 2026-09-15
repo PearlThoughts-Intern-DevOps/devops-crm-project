20c5d3dc37558bcda71bc7278c81aa7a46d7abfc
reddy8897 <ambureddy.1615@gmail.com>
2026-09-15 17:13:41 +0530

    feat: add Terraform ALB deployment for Twenty CRM

---
+++ task15/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+  default     = "ami-0b6d9d3d33ba97d99"
+}
