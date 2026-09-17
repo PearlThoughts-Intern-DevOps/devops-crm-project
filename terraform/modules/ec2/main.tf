391d74e32d769b29e97980ec3da7ae5d3e5b307c
rohithh55 <rohiithh5@gmail.com>
2026-09-17 17:06:22 +0530

    Create main.tf

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,66 @@
+resource "aws_security_group" "twenty_crm" {
+  name        = "twenty-crm-ansible-sg"
+  description = "Security group for Twenty CRM"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Twenty CRM"
+    from_port   = 2020
+    to_port     = 2020
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "twenty-crm-ansible-sg"
+    Project     = "twenty-crm"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+
+resource "aws_instance" "twenty_crm" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.twenty_crm.id
+  ]
+
+  tags = var.tags
+}
